b4623274412bf23b2e714c0a64928e49e2b289e9
Amanda Mattleman <amandamattleman@MSI>
2021-04-26 08:52:38 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+// console.log(ctx);
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src='shadow_dog.png';
+
+const spriteWidth = 575;
+const spriteHeight = 523;
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    // ctx.fillRect(50, 50, 100, 100);
+
+    ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    requestAnimationFrame(animate);
+};
+animate();
